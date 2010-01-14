f574d154e95267cae6e92d8afe37c47020bf6da8
Aaric Pittman <aaricpittman@aaric-pittmans-macbook-pro-15.local>
2010-01-14 05:53:01 -0600

    Added unpacked authlogic gem.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'authlogic', :version => '2.1.3'
   config.gem 'formtastic', :version => '0.9.7'
   
   config.time_zone = 'UTC'
